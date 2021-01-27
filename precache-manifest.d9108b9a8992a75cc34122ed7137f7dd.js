cad219e32fd8b54d6a99c6719f3068377837e709
awatson23 <awatson23@users.noreply.github.com>
2021-01-27 18:18:02 +0000

    deploy: c36085503f48a2618c0067b02f686640b5d6c9c0

---
+++ precache-manifest.d9108b9a8992a75cc34122ed7137f7dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9de68c14c3a5f109a76836c4f4826fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bccb5ef9d69b04d474c7",
+    "url": "/static/css/main.e98ed1b6.chunk.css"
+  },
+  {
+    "revision": "2d6d084539800276e6fd",
+    "url": "/static/js/2.2e767e96.chunk.js"
+  },
+  {
+    "revision": "107a53f487bc8e3a094f03dee1c9f700",
+    "url": "/static/js/2.2e767e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bccb5ef9d69b04d474c7",
+    "url": "/static/js/main.8e07b668.chunk.js"
+  },
+  {
+    "revision": "e2bc7bd2f95d88b3f2ac",
+    "url": "/static/js/runtime-main.9dcada38.js"
+  }
+]);
